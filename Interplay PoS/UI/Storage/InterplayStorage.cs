2186a6f26f320b01365a495fed9361e1510b3230
karthikeyan <karthikeyn@hotmail.com>
2019-03-27 17:49:37 +0530

    Code cleanup

--- Interplay PoS/UI/Storage/InterplayStorage.cs
+++ Interplay PoS/UI/Storage/InterplayStorage.cs
@@ -1,9 +1,7 @@
 ﻿using io.cloudloom.interplay.pos.Proxy.Contracts.Catalogue;
-using System;
+using Proxy.Services;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UI.Model;
 
 namespace UI.Storage
@@ -13,11 +11,82 @@ namespace UI.Storage
         static InterplayStorage()
         {
             Cart = new Cart();
+            CatalogueService service = new CatalogueService();
+            Catalogues = service.GetCatalogue()._embedded.catalogs;
         }
+
         public static List<Catalog> Catalogues { get; set; }
-        public static Catalog SelectedCatalog { get; set; }
-        public static ProductEntry SelectedProductEntry { get; set; }
-        public static SimpleArticle SelectedSimpleArticle { get; set; }
+        public static Catalog SelectedCatalog { get; private set; }
+        public static ProductEntry SelectedProductEntry { get; private set; }
+        public static SimpleArticle SelectedSimpleArticle { get; private set; }
         public static Cart Cart { get; set; }
+
+        public static List<ProductEntry> GetProductEntries()
+        {
+            List<ProductEntry> productEntries = new List<ProductEntry>();
+
+            Catalog catalog = InterplayStorage.Catalogues
+                .Where(catalogue => catalogue.name == InterplayStorage.SelectedCatalog.name).FirstOrDefault();
+
+            if (catalog != null)
+            {
+                productEntries = catalog.productEntries;
+            }
+
+            return productEntries;
+        }
+
+        public static List<ProductEntry> GetProductEntries(string productFilter)
+        {
+            return GetProductEntries().Where(product => product.name.ToLower().Contains(productFilter.ToLower())).ToList();
+        }
+
+        public static List<SimpleArticle> GetSimpleArticles()
+        {
+            List<SimpleArticle> simpleArticles = new List<SimpleArticle>();
+
+            Catalog catalogue = InterplayStorage.Catalogues.Where(cat => cat.name == InterplayStorage.SelectedCatalog.name).FirstOrDefault();
+
+            if (catalogue != null)
+            {
+                ProductEntry productEntry = catalogue.productEntries.Where(pe => pe.referenceProductId == InterplayStorage.SelectedProductEntry.referenceProductId).FirstOrDefault();
+                if (productEntry != null && productEntry.articleEntries != null)
+                {
+                    foreach (ArticleEntry article in productEntry.articleEntries)
+                    {
+                        if (article.simpleArticles != null)
+                        {
+                            simpleArticles.AddRange(article.simpleArticles);
+                        }
+                        else
+                        {
+                            simpleArticles.Add(new SimpleArticle
+                            {
+                                name = article.name,
+                            });
+                        }
+                    }
+                }
+            }
+
+            return simpleArticles;
+        }
+
+        public static void SetSelectedCatalog(string catalogName)
+        {
+            InterplayStorage.SelectedCatalog = InterplayStorage.Catalogues
+                .Where(catalogue => catalogue.name == catalogName).FirstOrDefault();
+        }
+
+        public static void SetSelectedProductEntry(ProductEntry selectedProduct)
+        {
+            SelectedProductEntry = selectedProduct;
+
+        }
+
+        public static void SetSelectedSimpleArticle(SimpleArticle selectedArticle)
+        {
+            SelectedSimpleArticle = selectedArticle;
+        }
     }
 }
